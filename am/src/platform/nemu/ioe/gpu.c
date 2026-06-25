2fa3dc24e7babbe7257183908a6dc48d5969a40e
SayatAbdikul <sayat.abdikul@nu.edu.kz>
2026-06-25 13:12:32 +0500

    Implement GPU IOE

--- am/src/platform/nemu/ioe/gpu.c
+++ am/src/platform/nemu/ioe/gpu.c
@@ -1,4 +1,5 @@
 #include <am.h>
+#include <klib.h>
 #include <nemu.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
@@ -7,14 +8,36 @@ void __am_gpu_init() {
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t config = inl(VGACTL_ADDR);
+  int width = config >> 16;
+  int height = config & 0xffff;
+
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = width, .height = height,
+    .vmemsz = width * height * (int)sizeof(uint32_t)
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  if (ctl->pixels != NULL) {
+    AM_GPU_CONFIG_T cfg;
+    __am_gpu_config(&cfg);
+
+    assert(ctl->x >= 0 && ctl->y >= 0);
+    assert(ctl->w >= 0 && ctl->h >= 0);
+    assert(ctl->x + ctl->w <= cfg.width);
+    assert(ctl->y + ctl->h <= cfg.height);
+
+    uint32_t *fb = (uint32_t *)FB_ADDR;
+    uint32_t *pixels = (uint32_t *)ctl->pixels;
+    for (int y = 0; y < ctl->h; y ++) {
+      memcpy(&fb[(ctl->y + y) * cfg.width + ctl->x],
+             &pixels[y * ctl->w],
+             ctl->w * sizeof(uint32_t));
+    }
+  }
+
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
