57496dc9703be84f5f879f20db106ed2271e5957
Sayat <127617769+SayatAbdikul@users.noreply.github.com>
2026-07-03 17:16:14 +0500

    changes to run the rt-thread on NPC

--- am/src/riscv/npc/cte.c
+++ am/src/riscv/npc/cte.c
@@ -8,6 +8,14 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
+      case 11:
+        if (c->GPR1 == (uintptr_t)-1) {
+          ev.event = EVENT_YIELD;
+          c->mepc += 4;
+        } else {
+          ev.event = EVENT_SYSCALL;
+        }
+        break;
       default: ev.event = EVENT_ERROR; break;
     }
 
@@ -31,7 +39,14 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  return NULL;
+  Context *c = (Context *)kstack.end - 1;
+  memset(c, 0, sizeof(Context));
+
+  c->mstatus = 0x1800;
+  c->mepc = (uintptr_t)entry;
+  c->gpr[10] = (uintptr_t)arg; // a0
+
+  return c;
 }
 
 void yield() {
