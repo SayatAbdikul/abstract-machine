e6b36fb43ef2d778689e909b074cb169f0fc9ebe
Sayat <127617769+SayatAbdikul@users.noreply.github.com>
2026-07-07 12:06:11 +0500

    Update timer.c

--- am/src/riscv/npc/timer.c
+++ am/src/riscv/npc/timer.c
@@ -1,15 +1,23 @@
 #include <am.h>
 #include <riscv/riscv.h>
 
-#define RTC_ADDR 0xa0000048
+#define RTC_ADDR 0xa0000048u
+#define NPC_CLINT_FREQ_HZ 500000000ull
 
 void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uint32_t hi = inl(RTC_ADDR + 4);
-  uint32_t lo = inl(RTC_ADDR);
-  uptime->us = ((uint64_t)hi << 32) | lo;
+  uint32_t hi1, hi2, lo;
+
+  do {
+    hi1 = inl(RTC_ADDR + 4);
+    lo = inl(RTC_ADDR);
+    hi2 = inl(RTC_ADDR + 4);
+  } while (hi1 != hi2);
+
+  uint64_t ticks = ((uint64_t)hi2 << 32) | lo;
+  uptime->us = ticks * 1000000ull / NPC_CLINT_FREQ_HZ;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
