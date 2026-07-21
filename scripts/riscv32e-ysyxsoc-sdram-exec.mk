2f2c4dd2308f98fc77dda31435805c02d033e786
Sayat <127617769+SayatAbdikul@users.noreply.github.com>
2026-07-21 19:46:25 +0500

    Add configurable SDRAM text alignment

--- scripts/riscv32e-ysyxsoc-sdram-exec.mk
+++ scripts/riscv32e-ysyxsoc-sdram-exec.mk
@@ -6,6 +6,10 @@ include $(AM_HOME)/scripts/platform/ysyxsoc.mk
 COMMON_CFLAGS += -march=rv32e_zicsr -mabi=ilp32e \
                  -DYSYXSOC_REPORT_MEMORY_LAYOUT=1
 LDFLAGS       += -melf32lriscv
+YSYXSOC_TEXT_PAD ?= 0
+# Define the sweep value before -T. GNU ld evaluates section addresses while
+# reading the linker script, so appending --defsym after -T is too late.
+LDFLAGS       := --defsym=PHASE11_TEXT_PAD=$(YSYXSOC_TEXT_PAD) $(LDFLAGS)
 
 AM_SRCS += riscv/npc/libgcc/div.S \
            riscv/npc/libgcc/muldi3.S \
